930f2b22e8c5d7c2f894c5e4e9a1002359d503a3
Ann Opeyemi <annopeyemi@gmail.com>
2023-12-28 20:51:21 -0500

    Adding user profile icon to log in page

--- src/routes/LoginForm.js
+++ src/routes/LoginForm.js
@@ -1,5 +1,8 @@
 import React from "react";
-import "../styles/Loginform.css"
+import "../styles/Loginform.css";
+import { Icon } from '@iconify/react';
+
+
 
 const LoginForm = () => {
     return (
@@ -8,16 +11,16 @@ const LoginForm = () => {
                 <h1>Login</h1>
                 <div className="names-aligned">
                     <p className="text-content">Username</p>
+                    <Icon icon="mdi:account-outline" className="icon" />
                     <input className="extended-input" type="text" placeholder="Type your username"/>
                 </div>
                 <div className="names-aligned">
                     <p className="text-content">Password</p>
-                    <input type="password" placeholder="Type your password"/>
+                    <input className="extended-input" type="password" placeholder="Type your password"/>
                 </div>
 
                 <p>Forgot password</p> 
-
-
+     
                 <p>Aren't a member yet?</p>
                 <button>Register here!</button>
             </div>
