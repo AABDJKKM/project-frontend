7cc99ac9e66027fdf28784795e8d9cb92fc01a9e
Ann Opeyemi <annopeyemi@gmail.com>
2024-03-05 13:22:18 -0500

    Updating register page

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import "../styles/RegisterStyles.css";
 
 const Register = () => {
     return(
         <div className="application">
             <form>
-               <label>First Name</label> 
+               <div className="input-boxes">
+                  <label className="text-Content">First Name</label>
+                  <input placeholder="Type First Name"></input>
+               </div> 
             </form>
             <form>
                <label>Last Name</label> 
